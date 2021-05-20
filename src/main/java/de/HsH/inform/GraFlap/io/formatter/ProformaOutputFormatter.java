75805130a4502b14d6f5a2d67155da1f3a12b38e
Mathias <mathias.sonderfeld@stud.hs-hannover.de>
2021-05-20 18:44:51 +0200

    Rewritten Proforma OutputFormatter
    Removed Jdom2 dependency from Proforma OutputFormatter and removed redundancy.

--- src/main/java/de/HsH/inform/GraFlap/io/formatter/ProformaOutputFormatter.java
+++ src/main/java/de/HsH/inform/GraFlap/io/formatter/ProformaOutputFormatter.java
@@ -1,10 +1,14 @@
 package de.HsH.inform.GraFlap.io.formatter;
 
 import de.HsH.inform.GraFlap.answer.Messages.AnswerMessage;
-import org.jdom2.Element;
-import org.jdom2.Namespace;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 /**
  * @author Mathias Sonderfeld
@@ -14,140 +18,143 @@ import org.jdom2.output.XMLOutputter;
  * requires Jdom-2.0.6 Library.
  */
 public class ProformaOutputFormatter implements OutputFormatter {
+    private String namespace = "proforma";
+    private Document document;
 
     /**
-     * converts AnswerMessage to XML in Proforma-Style with "proforma" as Namespace
-     * @param answerMessage the answerMessage to convert into XML
-     * @throws NullPointerException if input is null
-     * @return String containing the whole xml document
+     * creates a proforma xml from given AnswerMessage
+     * @param answerMessage the AnswerMessage containing the values
+     * @return xml as String
      */
-    public String format( AnswerMessage answerMessage ) {
-        XMLOutputter out = new XMLOutputter(Format.getPrettyFormat());
+    public String format(AnswerMessage answerMessage){
+        try{
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            document = db.newDocument();
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            StringWriter stringWriter = new StringWriter();
+
+            org.jdom2.output.XMLOutputter jdom2outputter = new org.jdom2.output.XMLOutputter(org.jdom2.output.Format.getPrettyFormat());
+            String svgAsString = jdom2outputter.outputString(answerMessage.getSvgImage());
+            buildProforma(answerMessage, svgAsString);
+            document.normalizeDocument();
+            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+            return stringWriter.toString();
+        }
+        catch(ParserConfigurationException | TransformerException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    /**
+     * builds the mainframe of the document
+     * @param answerMessage data
+     * @param svgAsString workaround because svgImage is Jdom2 Element
+     */
+    private void buildProforma(AnswerMessage answerMessage, String svgAsString){
+        Element response;
         if ("asvg".equals(answerMessage.getTaskMode())) {
-            return out.outputString(answerMessage.getSvgImage());
+            response = document.createElement(svgAsString);
+            document.appendChild(response);
+            return;
         }
 
-        Namespace proforma = Namespace.getNamespace("proforma", "urn:proforma:v2.1");
-        Namespace xs = Namespace.getNamespace("xs", "http://www.w3.org/2001/XMLSchema");
+        response = document.createElementNS("urn:proforma:v2.1","response");
+        response.setPrefix(namespace);
+        document.appendChild(response);
+        Element seperateTestFeedback = createElement(response, "separate-test-feedback");
+        createElement(seperateTestFeedback, "submission-feedback-list");
 
-        Element response = new Element("response");
-        response.setNamespace(proforma);
-
-
-        Element seperateTestFeedback = new Element("separate-test-feedback");
-        seperateTestFeedback.setNamespace(proforma);
-        response.addContent(seperateTestFeedback);
-
-        Element submissionFeedbackList = new Element("submission-feedback-list");
-        submissionFeedbackList.setNamespace(proforma);
-        seperateTestFeedback.addContent(submissionFeedbackList);
-
-        Element testsResponse = new Element("tests-response");
-        testsResponse.setNamespace(proforma);
-        seperateTestFeedback.addContent(testsResponse);
-
-        Element testResponse = new Element("test-response");
-        testResponse.setNamespace(proforma);
-        testResponse.setAttribute("id", answerMessage.getTaskTitle());
-        testsResponse.addContent(testResponse);
-
-        Element testResult = new Element("test-result");
-        testResult.setNamespace(proforma);
-        testResponse.addContent(testResult);
-
-        Element result = new Element("result");
-        result.setNamespace(proforma);
-        testResult.addContent(result);
-
-        double score = answerMessage.getPercentOfTestWordsFailed() == 0 ? 1 : 0;
-        Element testScore = new Element("score");
-        testScore.setNamespace(proforma);
-        testScore.addContent("" + score);
-        result.addContent(testScore);
-
-        Element testValidity = new Element("validity");
-        testValidity.setNamespace(proforma);
-        testValidity.addContent("1.0");
-        result.addContent(testValidity);
-
-        Element testFeedbackList = new Element("feedback-list");
-        testFeedbackList.setNamespace(proforma);
-        testResult.addContent(testFeedbackList);
-
-        Element studentFeedbackTitle = new Element("student-feedback");
-        studentFeedbackTitle.setNamespace(proforma);
-
-        Element studentFeedbackTitle_title = new Element("title");
-        studentFeedbackTitle_title.setNamespace(proforma);
-        studentFeedbackTitle_title.addContent("TaskTitle");
-        studentFeedbackTitle.addContent(studentFeedbackTitle_title);
-        Element studentFeedbackTitle_content = new Element("content");
-        studentFeedbackTitle_content.setAttribute("format", "plaintext");
-        studentFeedbackTitle_content.setNamespace(proforma);
-        studentFeedbackTitle_content.addContent(answerMessage.getTaskTitle());
-        studentFeedbackTitle.addContent(studentFeedbackTitle_content);
-        testFeedbackList.addContent(studentFeedbackTitle);
-
-        Element studentFeedbackTitleSVG = new Element("student-feedback");
-        studentFeedbackTitleSVG.setNamespace(proforma);
-        Element studentFeedbackTitleSVG_title = new Element("title");
-        studentFeedbackTitleSVG_title.setNamespace(proforma);
-        studentFeedbackTitleSVG_title.addContent("SvgTitle");
-        studentFeedbackTitleSVG.addContent(studentFeedbackTitleSVG_title);
-        Element studentFeedbackTitleSVG_content = new Element("content");
-        studentFeedbackTitleSVG_content.setAttribute("format", "plaintext");
-        studentFeedbackTitleSVG_content.setNamespace(proforma);
-        studentFeedbackTitleSVG_content.addContent(answerMessage.getSvgTitle());
-        studentFeedbackTitleSVG.addContent(studentFeedbackTitleSVG_content);
-        testFeedbackList.addContent(studentFeedbackTitleSVG);
-
-        Element studentFeedbackImageSVG = new Element("student-feedback");
-        studentFeedbackImageSVG.setNamespace(proforma);
-        Element studentFeedbackImageSVG_title = new Element("title");
-        studentFeedbackImageSVG_title.setNamespace(proforma);
-        studentFeedbackImageSVG_title.addContent("SvgImage");
-        studentFeedbackImageSVG.addContent(studentFeedbackImageSVG_title);
-        Element studentFeedbackImageSVG_content = new Element("content");
-        studentFeedbackImageSVG_content.setAttribute("format", "html");
-        studentFeedbackImageSVG_content.setNamespace(proforma);
-
-        Element svg = answerMessage.getSvgImage();
-        String escapedSvgImage = "";
-        if(svg != null){
-            escapedSvgImage = svg.getText();
+        Element testsResponse = createElement(seperateTestFeedback, "tests-response");
+        buildMainTestResponse(testsResponse, answerMessage, svgAsString);
+        if(answerMessage.getTaskMode().contains("a")){
+            buildSetsTestResponse(testsResponse, "states", true, "", "");
+            buildSetsTestResponse(testsResponse, "initials", true, "", "");
+            buildSetsTestResponse(testsResponse, "finals", true, "", "");
+            buildSetsTestResponse(testsResponse, "transitions", true, "", "");
         }
 
-        studentFeedbackImageSVG_content.addContent(escapedSvgImage);
-        studentFeedbackImageSVG.addContent(studentFeedbackImageSVG_content);
-        testFeedbackList.addContent(studentFeedbackImageSVG);
+        createElement(response, "files");
+        Element metaData = createElement(response, "response-meta-data");
+        Element graderEngine = createElement(metaData, "grader-engine");
+        graderEngine.setAttribute("name","GraFlap");
+        graderEngine.setAttribute("version", "0.3");
 
-        Element studentFeedbackResultText = new Element("student-feedback");
-        studentFeedbackResultText.setNamespace(proforma);
-        Element studentFeedbackResultText_title = new Element("title");
-        studentFeedbackResultText_title.setNamespace(proforma);
-        studentFeedbackResultText_title.addContent("ResultText");
-        studentFeedbackResultText.addContent(studentFeedbackResultText_title);
-        Element studentFeedbackResultText_content = new Element("content");
-        studentFeedbackResultText_content.setAttribute("format", "plaintext");
-        studentFeedbackResultText_content.setNamespace(proforma);
-        studentFeedbackResultText_content.addContent(answerMessage.getFeedback());
-        studentFeedbackResultText.addContent(studentFeedbackResultText_content);
-        testFeedbackList.addContent(studentFeedbackResultText);
+    }
 
-        Element files = new Element("files");
-        files.setNamespace(proforma);
-        response.addContent(files);
+    /**
+     * builds Test Response Segment for general feedback
+     */
+    private void buildMainTestResponse(Element testsResponse, AnswerMessage answerMessage, String svgAsString){
+        Element feedbackList = buildPartTestResponse(testsResponse, answerMessage.getTaskTitle(), answerMessage.hasPassed()?"1.0":"0.0", answerMessage.getPercentOfTestWordsFailed()>=0?"1.0":"0.0");
+        addFeedback(feedbackList, false, "Musterloesung", "plaintext", "", true); //answerMessage.getMusterloesung()
+        addFeedback(feedbackList, true, "TaskTitle", "plaintext", answerMessage.getTaskTitle(), false);
+        addFeedback(feedbackList, true, "SvgTitle", "plaintext", answerMessage.getSvgTitle(), false);
+        addFeedback(feedbackList, true, "SvGImage", "plaintext", svgAsString, true);
+        addFeedback(feedbackList, true, "FeedbackText", "plaintext", answerMessage.getFeedback(), false);
 
-        Element responseMetaData = new Element("response-meta-data");
-        responseMetaData.setNamespace(proforma);
-        response.addContent(responseMetaData);
+    }
 
-        Element graderEngine = new Element("grader-engine");
-        graderEngine.setNamespace(proforma);
-        graderEngine.setAttribute("name", "de.HsH.inform.GraFlap.GraFlap");
-        graderEngine.setAttribute("version", "0.1");
-        responseMetaData.addContent(graderEngine);
+    /**
+     * builds Test Response Segment for a Set Task Feedback
+     */
+    private void buildSetsTestResponse(Element testsResponse, String testTitle, boolean hasPassed ,String teacherFeedbackText, String studentFeedbackText){
+        Element feedbackList = buildPartTestResponse(testsResponse, testTitle, hasPassed?"1.0":"0.0", "1.0");
+        addFeedback(feedbackList, false, "ListOfMissing", "plaintext", teacherFeedbackText, false);
+        addFeedback(feedbackList,true, "Feedback", "plaintext", studentFeedbackText, false);
+    }
 
-        return out.outputString(response);
+    /**
+     * bundled to remove Redundancy
+     */
+    private Element buildPartTestResponse(Element testsResponse, String id, String scoreValue, String validityValue){
+        Element testResponse = createElement(testsResponse, "test-response", "id", id);
+        Element testResult = createElement(testResponse, "test-result");
+        Element result = createElement(testResult, "result");
+        Element score = createElement(result, "score");
+        score.appendChild(document.createTextNode(scoreValue));
+        Element validity = createElement(result, "validity");
+        validity.appendChild(document.createTextNode(validityValue));
+        Element feedbackList = createElement(testResult, "feedback-list");
+        return feedbackList;
+    }
+
+    /**
+     * adds a feedback-list element
+     */
+    private void addFeedback(Element feedbackList, boolean isStudentFeedback, String title, String formatValue, String content, boolean asCDATA){
+        Element feedback;
+        if(isStudentFeedback){
+            feedback = createElement(feedbackList, "student-feedback");
+        }
+        else{
+            feedback = createElement(feedbackList, "teacher-feedback");
+        }
+        Element fbTitle = createElement(feedback, "title");
+        fbTitle.appendChild(document.createTextNode(title));
+
+        Element fbContent = createElement(feedback, "content", "format", formatValue);
+        if(asCDATA){
+            fbContent.appendChild(document.createCDATASection(content));
+        }
+        else{
+            fbContent.appendChild(document.createTextNode(content));
+        }
+    }
+
+
+    private Element createElement(Element parent, String name){
+        Element e = document.createElementNS("urn:proforma:v2.1",name);
+        e.setPrefix(namespace);
+        parent.appendChild(e);
+        return e;
+    }
+
+    private Element createElement(Element parent, String name, String attributeName, String attributeValue){
+        Element e = createElement(parent, name);
+        e.setAttribute(attributeName, attributeValue);
+        return e;
     }
 }
