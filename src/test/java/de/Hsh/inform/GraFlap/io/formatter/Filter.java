fb84abe7b358dc5f14db8cb43edd36a43fe213df
Mathias Sonderfeld <mathias.sonderfeld@stud.hs-hannover.de>
2021-09-15 18:01:54 +0200

    Hopefully will this fix UNIX Linebreak issues

--- src/test/java/de/Hsh/inform/GraFlap/io/formatter/Filter.java
+++ src/test/java/de/Hsh/inform/GraFlap/io/formatter/Filter.java
@@ -2,6 +2,6 @@ package de.HsH.inform.GraFlap.io.formatter;
 
 public class Filter {
     public static String filter(String in){
-        return in.replaceAll("datetime=\"[0-9]*\"", "datetime=\"\"").replaceAll("\"GraFlap\" version=\"[0-9.]*\"", "\"GraFlap\" version=\"\"").replaceAll("\\s{2,}", "").trim();
+        return in.replaceAll("\t","").replaceAll("\r","").replaceAll("\n","").replaceAll("datetime=\"[0-9]*\"", "datetime=\"\"").replaceAll("\"GraFlap\" version=\"[0-9.]*\"", "\"GraFlap\" version=\"\"").replaceAll("\\s{2,}", "").trim();
     }
 }
